9c98f71ca41dcd39586c91fd7705720477bf88af
fatinAlbustanji <86020993+fatinAlbustanji@users.noreply.github.com>
2021-08-09 20:19:18 +0300

    Add files via upload

---
+++ movesvalues.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 08 أغسطس 2021 الساعة 03:04
+-- إصدار الخادم: 8.0.17
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `movesdatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `movesvalues`
+--
+
+CREATE TABLE `movesvalues` (
+  `id` int(11) NOT NULL,
+  `ForwardBtn` varchar(3) NOT NULL,
+  `BackwardBtn` varchar(3) NOT NULL,
+  `LeftBtn` varchar(3) NOT NULL,
+  `RightBtn` varchar(3) NOT NULL,
+  `StopBtn` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
